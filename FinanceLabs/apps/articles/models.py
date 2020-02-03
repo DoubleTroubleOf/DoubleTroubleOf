1deac11af589eb684bd4275365bfea0bfe55a5e5
DoubleTroubleOf <vitalik.tustanovskij@gmail.com>
2020-02-03 16:43:20 +0200

    init

---
+++ FinanceLabs/apps/articles/models.py
@@ -0,0 +1,31 @@
+import datetime
+from django.db import models
+
+from django.utils import timezone
+
+class Article(models.Model):
+    article_title = models.CharField('Назва статті', max_length=100)
+    article_text =  models.TextField('Текст статті')
+    pub_date = models.DateTimeField('Дата публікації')
+
+    def __str__(self):
+        return self.article_title
+
+    def was_published_recently(self):
+        return self.pub_date >= (timezone.now() - datetime.timedelta(days=7))
+
+    class Meta:
+        verbose_name = 'Стаття'
+        verbose_name_plural = 'Статті'
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    author_name = models.CharField('Ім*я автора', max_length=50)
+    comment_text = models.CharField('Текст коментаря', max_length=200)
+
+    def __str__(self):
+        return self.author_name
+
+    class Meta:
+        verbose_name = 'Коментарій'
+        verbose_name_plural = 'Коментарі'
